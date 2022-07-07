c45ea9ec4fc4211219b82ad2e895cb2fc327485f
Ícaro Cristolfe <cristolfeicaro@gmail.com>
2022-07-07 18:58:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import { FaGithub,FaWhatsapp,FaLinkedin,FaFilePdf } from 'react-icons/fa';
+
+import {
+  Body,
+  Container
+} from './styles'
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+
+   
+
+      <Container>
+      <h1>My Links</h1>
+      <div> 
+      <button><a href="https://wa.me/+5527997679661" target="_blanked"><FaWhatsapp/><br/>Whatsapp</a></button>
+      </div>
+
+      <div> 
+      <button><a href="https://www.linkedin.com/in/%C3%ADcaro-cristolfe-0b8104197/" target="_blanked"><FaLinkedin/><br/>LinkedIn</a></button>
+      </div>
+      
+      <div> 
+      <button><a href="https://github.com/cristolfe-prog" target="_blanked"><FaGithub/> <br/>Github</a></button>
+      </div>
+
+      <div> 
+      <button><a href="https://drive.google.com/file/d/15M4TiwlQmJeqRMGIkQEi0GQUU1GXyzEQ/view?usp=sharing" target="_blanked"><FaFilePdf/> <br/>Currículo</a></button>
+      </div>
+
+      </Container>
+    </Body>
   );
 }
 
